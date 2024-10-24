a9cc4d873afd4720b0f6a27015dbbbb28a3e9366
anthonysfonseca <anthonyfonseca00@gmail.com>
2024-10-23 17:01:36 -0700

    Lab 10 - 10.3) Exercise 1

---
+++ readata.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Anthony Fonseca on 10/23/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char last[10];
+
+    cout << "Please enter your last name using no more than 9 characters:" << endl;
+    cin >> last;
+
+    cout << "Name entered: " << last << endl;
+
+    return 0;
+}
